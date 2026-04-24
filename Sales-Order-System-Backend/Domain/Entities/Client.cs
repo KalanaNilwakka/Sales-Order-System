d5721723fb030bf24a5ae33d0f1451875bef56ed
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-24 12:48:54 +0530

    Created and implemented Client entitiy

---
+++ Sales-Order-System-Backend/Domain/Entities/Client.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales_Order_System_Backend.Domain.Entities;
+
+public class Client
+{
+    [Key]
+    public long Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public string Address1 { get; set; }
+    public string Address2 { get; set; }
+    public string Address3 { get; set; }
+    public string Suburb { get; set; }
+    public string State { get; set; }
+    public int PostCode { get; set; }
+    
+    public List<Order> Orders { get; set; }
+}
