1f4ab1feed6d717dbd4a9ee0ef04369a88b042f1
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-24 17:16:15 +0530

    Implemented methods from the interface of Item repository

---
+++ Sales-Order-System-Backend/Infrastructure/Repositories/Implementations/ItemRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales_Order_System_Backend.Domain.Entities;
+using Sales_Order_System_Backend.Infrastructure.Data;
+using Sales_Order_System_Backend.Infrastructure.Repositories.Interfaces;
+
+namespace Sales_Order_System_Backend.Infrastructure.Repositories.Implementations;
+
+public class ItemRepository : IItemRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public ItemRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<List<Item>> GetAllAsync()
+    {
+        return await _dbContext.Items.ToListAsync();
+    }
+
+    public async Task<Item> GetByIdAsync(int id)
+    {
+        return await _dbContext.Items.FirstOrDefaultAsync(i => i.Id == id);
+    }
+}
