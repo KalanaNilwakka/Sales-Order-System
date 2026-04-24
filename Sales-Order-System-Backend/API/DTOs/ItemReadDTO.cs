26db4b7ede66e3bfab7b0c76f217974d27ebb9d4
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-24 13:42:06 +0530

    Created and implemented DTO needed for Item entity

---
+++ Sales-Order-System-Backend/API/DTOs/ItemReadDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Sales_Order_System_Backend.API.DTOs;
+
+public class ItemReadDTO
+{
+    public int Id { get; set; }
+    public string Code { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+}
