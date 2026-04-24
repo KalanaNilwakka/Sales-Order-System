0d60b740cdeaae1d336880a20bd4cad5b2188a39
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-24 17:15:55 +0530

    Implemented methods from the interface of Client repository

---
+++ Sales-Order-System-Backend/Infrastructure/Repositories/Implementations/ClientRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales_Order_System_Backend.Domain.Entities;
+using Sales_Order_System_Backend.Infrastructure.Data;
+using Sales_Order_System_Backend.Infrastructure.Repositories.Interfaces;
+
+namespace Sales_Order_System_Backend.Infrastructure.Repositories.Implementations;
+
+public class ClientRepository : IClientRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public ClientRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<List<Client>> GetAllAsync()
+    {
+        return await _dbContext.Clients.ToListAsync();
+    }
+
+    public async Task<Client> GetByIdAsync(int id)
+    {
+        return await _dbContext.Clients.FirstOrDefaultAsync(c => c.Id == id);
+    }
+}
