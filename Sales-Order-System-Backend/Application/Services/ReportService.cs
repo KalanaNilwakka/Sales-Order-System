e32b9e51d13d5d3b34ea4b7fa6d8ea63044c3fbf
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-24 23:43:35 +0530

    Created and implemented the service layer for PDF generation

---
+++ Sales-Order-System-Backend/Application/Services/ReportService.cs
@@ -0,0 +1,15 @@
+﻿using QuestPDF.Fluent;
+using Sales_Order_System_Backend.Application.Interfaces;
+using Sales_Order_System_Backend.Domain.Entities;
+using Sales_Order_System_Backend.Reports;
+
+namespace Sales_Order_System_Backend.Application.Services;
+
+public class ReportService : IReportService
+{
+    public byte[] GenerateOrderInvoice(Order order)
+    {
+        var document = new OrderInvoiceDocument(order);
+        return document.GeneratePdf();
+    }
+}
