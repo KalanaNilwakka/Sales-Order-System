de912ac1820737bd3819f48be967b1231e836f1b
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-24 13:42:28 +0530

    Created and implemented DTO needed for OrderItem entity

---
+++ Sales-Order-System-Backend/API/DTOs/OrderItemCreateDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales_Order_System_Backend.API.DTOs;
+
+public class OrderItemCreateDTO
+{
+    [Required]
+    public int OrderId { get; set; }
+    [Required]
+    public int ItemId { get; set; }
+    public string Description { get; set; }
+    public string Note { get; set; }
+    [Required]
+    public int Quantity { get; set; }
+    [Required]
+    public decimal Price { get; set; }
+    [Required]
+    public decimal TaxRate { get; set; }
+}
