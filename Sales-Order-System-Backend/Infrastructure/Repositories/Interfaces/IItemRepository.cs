6361f7306212f55f746e286314c6617147cf6fbf
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-24 16:45:01 +0530

    Created and implemented the interface for the Item repository

---
+++ Sales-Order-System-Backend/Infrastructure/Repositories/Interfaces/IItemRepository.cs
@@ -0,0 +1,9 @@
+﻿using Sales_Order_System_Backend.Domain.Entities;
+
+namespace Sales_Order_System_Backend.Infrastructure.Repositories.Interfaces;
+
+public interface IItemRepository
+{
+    Task<List<Item>> GetAllAsync();
+    Task<Item> GetByIdAsync(int id);
+}
