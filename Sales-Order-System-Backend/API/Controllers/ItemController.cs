9426261dbc40788da6054091496d63a73453d322
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-24 20:43:36 +0530

    Created and implemented ItemController with the necessary endpoints

---
+++ Sales-Order-System-Backend/API/Controllers/ItemController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sales_Order_System_Backend.API.DTOs;
+using Sales_Order_System_Backend.Application.Interfaces;
+
+namespace Sales_Order_System_Backend.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ItemController : ControllerBase
+{
+    private readonly IItemService _service;
+
+    public ItemController(IItemService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ItemReadDTO>>> GetAllAsync()
+    {
+        var items = await _service.GetAllAsync();
+        return Ok(items);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ItemReadDTO>> GetByIdAsync(int id)
+    {
+        var item = await _service.GetByIdAsync(id);
+        if (item == null)
+            return NotFound();
+        return Ok(item);
+    }
+}
