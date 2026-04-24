b00072d12f1f6c2aceeb238fac292b74341fdd30
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-24 12:55:50 +0530

    Created and implemented AppDbContext to create the DB

---
+++ Sales-Order-System-Backend/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Sales_Order_System_Backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sales_Order_System_Backend.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Item> Items { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+}
