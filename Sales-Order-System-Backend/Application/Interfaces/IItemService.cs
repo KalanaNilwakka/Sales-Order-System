f00a7f5cb867c6ca46e1cbc0dbb317787e038970
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-24 15:07:41 +0530

    Created and implemented the interface for ItemService

---
+++ Sales-Order-System-Backend/Application/Interfaces/IItemService.cs
@@ -0,0 +1,10 @@
+﻿using Sales_Order_System_Backend.API.DTOs;
+
+namespace Sales_Order_System_Backend.Application.Interfaces;
+
+public interface IItemService
+{
+    Task<List<ItemReadDTO>> GetAllAsync();
+
+    Task<ItemReadDTO> GetByIdAsync(int id);
+}
