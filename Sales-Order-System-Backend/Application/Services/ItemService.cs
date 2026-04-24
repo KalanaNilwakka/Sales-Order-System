d52ad65cce5cbabc61ce26fefd28f2af323f04e0
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-24 16:16:48 +0530

    Implemented methods from the interface of Item entity

---
+++ Sales-Order-System-Backend/Application/Services/ItemService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Sales_Order_System_Backend.API.DTOs;
+using Sales_Order_System_Backend.Application.Interfaces;
+using Sales_Order_System_Backend.Infrastructure.Data;
+
+namespace Sales_Order_System_Backend.Application.Services;
+
+public class ItemService : IItemService
+{
+    private readonly AppDbContext _dbContext;
+    private readonly IMapper _mapper;
+
+    public ItemService(AppDbContext dbContext, IMapper mapper)
+    {
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
+    
+    public async Task<List<ItemReadDTO>> GetAllAsync()
+    {
+        var items = await _dbContext.Items
+            .ToListAsync();
+        
+        return _mapper.Map<List<ItemReadDTO>>(items);
+    }
+
+    public async Task<ItemReadDTO> GetByIdAsync(int id)
+    {
+        var item = await _dbContext.Items
+            .FindAsync(id);
+
+        if (item == null)
+            throw new KeyNotFoundException($"Item with ID {id} was not found.");
+
+        return _mapper.Map<ItemReadDTO>(item);
+    }
+}
