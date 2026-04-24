1afabbc663a2a145ef20c7dbeadc32110756fe29
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-24 15:07:56 +0530

    Created and implemented the interface for OrderService

---
+++ Sales-Order-System-Backend/Application/Interfaces/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using Sales_Order_System_Backend.API.DTOs;
+
+namespace Sales_Order_System_Backend.Application.Interfaces;
+
+public interface IOrderService
+{
+    Task<List<OrderReadDTO>> GetAllAsync();
+
+    Task<OrderReadDTO> GetByIdAsync(int id);
+
+    Task<OrderReadDTO> CreateAsync(OrderCreateDTO dto);
+}
