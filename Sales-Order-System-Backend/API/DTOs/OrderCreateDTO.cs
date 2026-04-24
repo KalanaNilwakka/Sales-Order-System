750563fe01da931993db16ac1c0b3bca23dc79e0
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-24 13:42:43 +0530

    Created and implemented DTO needed for Order entity

---
+++ Sales-Order-System-Backend/API/DTOs/OrderCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales_Order_System_Backend.API.DTOs;
+
+public class OrderCreateDTO
+{
+    [Required]
+    public int ClientId { get; set; }
+    [Required]
+    public string InvoiceNo { get; set; }
+    public DateTime InvoiceDate { get; set; }
+    public string ReferenceNo { get; set; }
+    
+    public List<OrderItemCreateDTO> Items { get; set; }
+}
---
+++ Sales-Order-System-Backend/API/DTOs/OrderReadDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Sales_Order_System_Backend.API.DTOs;
+
+public class OrderReadDTO
+{
+    public int Id { get; set; }
+    public int ClientId { get; set; }
+    public string InvoiceNo { get; set; }
+    public DateTime InvoiceDate { get; set; }
+    public string ReferenceNo { get; set; }
+    public decimal TotalExcl { get; set; }
+    public decimal TotalTax { get; set; }
+    public decimal TotalIncl { get; set; }
+
+    public List<OrderItemReadDTO> Items { get; set; }
+}
