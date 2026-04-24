7d430d784cd5b63aa194802051d2a9ed56acde7c
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-24 13:50:36 +0530

    Created and implemented MappingProfile file for DTO mapping

---
+++ Sales-Order-System-Backend/Mappings/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Sales_Order_System_Backend.API.DTOs;
+using Sales_Order_System_Backend.Domain.Entities;
+
+namespace Sales_Order_System_Backend.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Client, ClientReadDTO>();
+
+        CreateMap<Item, ItemReadDTO>();
+
+        CreateMap<Order, OrderReadDTO>();
+        CreateMap<OrderCreateDTO, Order>();
+
+        CreateMap<OrderItem, OrderItemReadDTO>();
+        CreateMap<OrderItemCreateDTO, OrderItem>();
+    }
+}
