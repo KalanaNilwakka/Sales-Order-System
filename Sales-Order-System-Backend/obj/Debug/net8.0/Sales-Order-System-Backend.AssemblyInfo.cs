e4afaee2d3536b71e3744862e56991f8700b27b2
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-24 23:39:58 +0530

    Added the package QuestPDF for PDF generation

--- Sales-Order-System-Backend/obj/Debug/net8.0/Sales-Order-System-Backend.AssemblyInfo.cs
+++ Sales-Order-System-Backend/obj/Debug/net8.0/Sales-Order-System-Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sales-Order-System-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de865d5a384684f7a3cced2b502331918169048a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d76aa5a63aa120cc5d359c2c8d46cd67b0f5cf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sales-Order-System-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sales-Order-System-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
